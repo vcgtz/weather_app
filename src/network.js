706f0f908e3b6402f38a5a1ac1660824a8df6b43
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-05-17 16:32:13 -0500

    Use a .env file to store urls and api keys

--- src/network.js
+++ src/network.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 
 const getMyPublicIPAddress = async () => {
     try {
-        const response = await axios.get('https://api.ipify.org?format=json');
+        const response = await axios.get(process.env.IPIFY_URL);
 
         if (response.status !== 200) {
             throw new Error(`Status Code: ${response.status}. ${response.statusText}`);
